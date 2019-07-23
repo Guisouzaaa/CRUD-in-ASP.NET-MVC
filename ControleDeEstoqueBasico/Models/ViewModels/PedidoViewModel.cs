fff96af56f71d122bd3a6b6352d8e58c2bd9ccab
Guisouzaaa <guilhermesouzafe@gmail.com>
2019-07-22 23:57:47 -0300

    Update

---
+++ ControleDeEstoqueBasico/Models/ViewModels/PedidoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDControleDeEstoque.Models.ViewModels
+{
+    public class PedidoViewModel
+    {
+        public int Id { get; set; }
+        public int Cliente_Id { get; set; }
+        public int Produto_Id { get; set; }
+        public int Quantidade { get; set; }
+
+    }
+}
